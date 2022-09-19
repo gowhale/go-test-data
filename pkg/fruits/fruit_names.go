6d61b5f6572830f456bb8a9c81dbde535e92c696
Gabe <32711718+gowhale@users.noreply.github.com>
2022-09-19 01:18:26 +0100

    feat(fruit): Add some fruits #1

---
+++ pkg/fruits/fruit_names.go
@@ -0,0 +1,33 @@
+// Package fruit contains fruit names for use in testing
+package fruit
+
+// Fruit names to use for testing
+const (
+	Apple       = "Apple"
+	Watermelon  = "Watermelon"
+	Orange      = "Orange"
+	Pear        = "Pear"
+	Cherry      = "Cherry"
+	Strawberry  = "Strawberry"
+	Nectarine   = "Nectarine"
+	Grape       = "Grape"
+	Mango       = "Mango"
+	Blueberry   = "Blueberry"
+	Pomegranate = "Pomegranate"
+	Plum        = "Plum"
+	Banana      = "Banana"
+	Raspberry   = "Raspberry"
+	Mandarin    = "Mandarin"
+	Jackfruit   = "Jackfruit"
+	Papaya      = "Papaya"
+	Kiwi        = "Kiwi"
+	Pineapple   = "Pineapple"
+	Lime        = "Lime"
+	Lemon       = "Lemon"
+	Apricot     = "Apricot"
+	Grapefruit  = "Grapefruit"
+	Melon       = "Melon"
+	Coconut     = "Coconut"
+	Avocado     = "Avocado"
+	Peach       = "Peach"
+)
